cf64b123b2541266c446d33c5957fc536765c620
허세진 <heosejin222@gmail.com>
2025-05-09 00:39:04 +0900

    Add : CommentRequestDto에 유효성 검증 추가(Size)

--- src/main/java/com/Tera/blog/domain/comment/dto/CommentRequestDto.java
+++ src/main/java/com/Tera/blog/domain/comment/dto/CommentRequestDto.java
@@ -1,8 +1,10 @@
 package com.Tera.blog.domain.comment.dto;
 
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class CommentRequestDto {
+    @Size(min = 1, message = "내용 1글자 이상 써주세요.")
     private String content;
 }
